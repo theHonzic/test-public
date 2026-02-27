515de851dff355891dd69edd19a155037827049c
Jan Janovec <99410175+theHonzic@users.noreply.github.com>
2026-02-27 10:40:10 +0100

    v2.0.3

--- Package.swift
+++ Package.swift
@@ -42,17 +42,17 @@ let package = Package(
         ),
         .binaryTarget(
             name: "MinimalPackageBinary",
-            url: "https://github.com/theHonzic/test-public/releases/download/2.0.1/MinimalPackage.xcframework.zip",
-            checksum: "490bcb887907e58dc963a1c6d967a93280b6da673d13a94ba947aff1cad1c71f"
+            url: "https://github.com/theHonzic/test-public/releases/download/2.0.3/MinimalPackage.xcframework.zip",
+            checksum: "b91edd53aed8a3a585daded58d86c8bcec07d9ff04aa803ef7035200522331d3"
         ),
         .binaryTarget(
             name: "MinimalPackageCoreBinary",
-            url: "https://github.com/theHonzic/test-public/releases/download/2.0.1/MinimalPackageCore.xcframework.zip",
+            url: "https://github.com/theHonzic/test-public/releases/download/2.0.3/MinimalPackageCore.xcframework.zip",
             checksum: "PLACEHOLDER_CORE"
         ),
         .binaryTarget(
             name: "MinimalPackageFeatureBinary",
-            url: "https://github.com/theHonzic/test-public/releases/download/2.0.1/MinimalPackageFeature.xcframework.zip",
+            url: "https://github.com/theHonzic/test-public/releases/download/2.0.3/MinimalPackageFeature.xcframework.zip",
             checksum: "PLACEHOLDER_FEATURE"
         ),
     ]
