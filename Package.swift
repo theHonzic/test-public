39b03b65933c1fea01c2efdf43ecce404b9e9e9a
Jan Janovec <99410175+theHonzic@users.noreply.github.com>
2026-03-02 07:25:57 +0100

    release: v2.3.8

--- Package.swift
+++ Package.swift
@@ -1,59 +1,22 @@
-// swift-tools-version: 6.2
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "MinimalPackage",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13)
+        .iOS(.v26)
     ],
     products: [
         .library(
             name: "MinimalPackage",
-            targets: ["MinimalPackageTarget"]
-        ),
-    ],
-    dependencies: [
-        // External runtime dependencies required by the binary
-        .package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.5.2")),
-        .package(url: "https://github.com/dagronf/qrcode.git", .upToNextMajor(from: "27.11.0")),
-        .package(url: "https://github.com/apple/swift-openapi-runtime.git", .upToNextMajor(from: "1.8.3")),
-        .package(url: "https://github.com/apple/swift-openapi-urlsession.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "24.0.0")),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.40.0"),
+            targets: ["MinimalPackage"]
+        )
     ],
     targets: [
-        // Thin wrapper that links the binary frameworks and their runtime deps
-        .target(
-            name: "MinimalPackageTarget",
-            dependencies: [
-                "MinimalPackageBinary",
-                "MinimalPackageCoreBinary",
-                "MinimalPackageFeatureBinary",
-                .product(name: "Lottie", package: "lottie-spm"),
-                .product(name: "QRCode", package: "qrcode"),
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
-                .product(name: "KeychainSwift", package: "keychain-swift"),
-                .product(name: "Sentry", package: "sentry-cocoa"),
-            ],
-            path: "Sources/MinimalPackageTarget"
-        ),
         .binaryTarget(
-            name: "MinimalPackageBinary",
-            url: "https://github.com/theHonzic/test-public/releases/download/2.0.6/MinimalPackage.xcframework.zip",
-            checksum: "10acc9f714dff94b178bbaeb07705dc3062b6f57e538a1562b097a10882236f1"
-        ),
-        .binaryTarget(
-            name: "MinimalPackageCoreBinary",
-            url: "https://github.com/theHonzic/test-public/releases/download/2.0.6/MinimalPackageCore.xcframework.zip",
-            checksum: "PLACEHOLDER_CORE"
-        ),
-        .binaryTarget(
-            name: "MinimalPackageFeatureBinary",
-            url: "https://github.com/theHonzic/test-public/releases/download/2.0.6/MinimalPackageFeature.xcframework.zip",
-            checksum: "PLACEHOLDER_FEATURE"
-        ),
+            name: "MinimalPackage",
+            url: "https://github.com/theHonzic/test-public/releases/download/v2.3.8/MinimalPackage.xcframework.zip",
+            checksum: "e6eb76561fd619f204b2ff21e45ad22eccfeca526b67795d70befb943433d5ec"
+        )
     ]
 )
