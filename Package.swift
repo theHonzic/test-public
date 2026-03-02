f1445e07a6168bb5447793742b3c9f58f427d221
Jan Janovec <99410175+theHonzic@users.noreply.github.com>
2026-03-02 09:40:39 +0100

    release: v2.3.12

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MinimalPackage",
-            url: "https://github.com/theHonzic/test-public/releases/download/v2.3.11/MinimalPackage.xcframework.zip",
-            checksum: "02a50a791cbb5e60279d3e96d49e88b39cb05fdcfa3871b4df9aec75105c9527"
+            url: "https://github.com/theHonzic/test-public/releases/download/v2.3.12/MinimalPackage.xcframework.zip",
+            checksum: "0c22d4766df0d070433db9665dd8e4e4d85c8e2b80df4a3fbdcef7c8a68ab6b4"
         )
     ]
 )
